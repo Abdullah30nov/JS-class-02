fe8358601f236be6bf28ac28af7ed8c804275da0
Abdullah30nov <fatimaaiman248@gmail.com>
2024-02-22 21:12:18 +0500

    JAVASCRIPT CLASS - 02 CODE

---
+++ aap.js
@@ -0,0 +1,14 @@
+      document.write(
+        "<table><h1>TABLE OF FOUR</h1> <br> 4 x 1 = 4  <br> 4 x 2 = 8  <br> 4 x 3 = 12  <br> 4 x 4 = 16  <br> 4 x 5 = 20  <br> 4 x 6 = 24  <br> 4 x 7 = 28  <br> 4 x 8 = 32  <br> 4 x 9 = 36  <br> 4 x 10 = 40 </table> <br>"
+      );
+
+      var number = 46;
+
+     var cal = --number +  ++number + number - --number + ++number - number - --number + ++number + number ;
+     console.log(cal);
+       var name = "abdullah", education = "Enter", Rollno = "67612", section= "A";
+     document.write("<h1>BIO DATA</h1></h2>" + " " + "My name is: " + name + "<br>" + "I complete: " + education +  "<br>" + "My RollNo is:" + Rollno +  "<br>"  + "My section is:" + section);
+
+
+     var number = 5;
+     
